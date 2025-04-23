e4fa2540b4437cfe61118d790050a035127b6c45
uppiliy <uppiliymuralidharan98@gmail.com>
2025-04-23 11:09:11 +0530

    initial commit of the welcome web app

---
+++ app.py
@@ -0,0 +1,20 @@
+# app.py
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        version = "1.0.1"
+        return render_template_string("<h1>Welcome to new Version: {{ version }}, {{ name }}</h1>", version=version, name=name)
+    return '''
+        <form method="post">
+            Enter your name: <input type="text" name="name">
+            <input type="submit" value="Submit">
+        </form>
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
